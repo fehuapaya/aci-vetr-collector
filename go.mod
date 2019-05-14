8ea3cdadee9c990f6ca47c37e563f3e7ef8f851e
Nathan Hemingway <nhemingway@gmail.com>
2019-05-14 15:19:51 -0700

    move result to single file

--- go.mod
+++ go.mod
@@ -10,5 +10,6 @@ require (
 	github.com/mholt/archiver v3.1.1+incompatible
 	github.com/nwaples/rardecode v1.0.0 // indirect
 	github.com/pierrec/lz4 v2.0.5+incompatible // indirect
+	github.com/tidwall/sjson v1.0.4
 	github.com/xi2/xz v0.0.0-20171230120015-48954b6210f8 // indirect
 )
--- main.go
+++ main.go
@@ -7,23 +7,31 @@ import (
 	"log"
 	"os"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/alexflint/go-arg"
 	"github.com/brightpuddle/go-aci"
 	"github.com/mholt/archiver"
+	"github.com/tidwall/sjson"
 )
 
-const schemaVersion = 6
-const version = "0.1.7"
-const resultZip = "health-check-data.zip"
+const (
+	schemaVersion = 6
+	version       = "0.1.7"
+	resultZip     = "health-check-data.zip"
+	resultFile    = "data.json"
+)
+
+var wg sync.WaitGroup
+var mutex = &sync.Mutex{}
+var out []byte
 
 // Config : command line parameters
 type Config struct {
 	IP       string `arg:"-i" help:"APIC IP address"`
 	Username string `arg:"-u"`
 	Password string `arg:"-p"`
-	Pretty   bool   `help:"pretty print JSON"`
 }
 
 // Description : CLI description string
@@ -42,93 +50,244 @@ func newConfigFromCLI() (cfg Config) {
 }
 
 type request struct {
-	name  string
-	class string
-	query []string
+	name   string
+	class  string
+	query  []string
+	filter string
 }
 
 var reqs = []request{
+
 	// Tenant objects
-	request{name: "bds", class: "fvBD"},
-	request{name: "contracts", class: "vzBrCP"},
-	request{name: "epgs", class: "fvEpP"},
-	request{name: "ext-epgs", class: "l3extInstP"},
-	request{name: "filters", class: "vzRsSubjFiltAtt"},
-	request{name: "l3-int-profiles", class: "l3extLIfP"},
-	request{name: "l3-node-profiles", class: "l3extLNodeP"},
-	request{name: "l3outs", class: "l3extOut"},
-	request{name: "subjects", class: "vzSubj"},
-	request{name: "tenants", class: "fvTenant"},
-	request{name: "vrfs", class: "fvCtx"},
+	request{
+		name:   "bds",
+		class:  "fvBD",
+		filter: "#.fvBD.attributes",
+	},
+	request{
+		name:   "contracts",
+		class:  "vzBrCP",
+		filter: "#.vzBrCP.attributes",
+	},
+	request{
+		name:   "epgs",
+		class:  "fvEpP",
+		filter: "#.fvEpP.attributes",
+	},
+	request{
+		name:   "ext-epgs",
+		class:  "l3extInstP",
+		filter: "#.l3extInstP.attributes",
+	},
+	request{
+		name:   "filters",
+		class:  "vzRsSubjFiltAtt",
+		filter: "#.vzRsSubjFiltAtt.attributes",
+	},
+	request{
+		name:   "l3-int-profiles",
+		class:  "l3extLIfP",
+		filter: "#.l3extLIfP.attributes",
+	},
+	request{
+		name:   "l3-node-profiles",
+		class:  "l3extLNodeP",
+		filter: "#.l3extLNodeP.attributes",
+	},
+	request{
+		name:   "l3outs",
+		class:  "l3extOut",
+		filter: "#.l3extOut.attributes",
+	},
+	request{
+		name:   "subjects",
+		class:  "vzSubj",
+		filter: "#.vzSubj.attributes",
+	},
+	request{
+		name:   "tenants",
+		class:  "fvTenant",
+		filter: "#.fvTenant.attributes",
+	},
+	request{
+		name:   "vrfs",
+		class:  "fvCtx",
+		filter: "#.fvCtx.attributes",
+	},
 
 	// Infrastructure
-	request{name: "apic-hardware", class: "eqptBoard"},
-	request{name: "devices", class: "topSystem"},
-	request{name: "pods", class: "fabricSetupP"},
-	request{name: "switch-hardware", class: "fabricNode"},
+	request{
+		name:   "hardware-apic",
+		class:  "eqptBoard",
+		filter: "#.eqptBoard.attributes",
+	},
+	request{
+		name:   "devices",
+		class:  "topSystem",
+		filter: "#.topSystem.attributes",
+	},
+	request{
+		name:   "pods",
+		class:  "fabricSetupP",
+		filter: "#.fabricSetupP.attributes",
+	},
+	request{
+		name:   "hardware-switch",
+		class:  "fabricNode",
+		filter: "#.fabricNode.attributes",
+	},
 
 	// State
-	request{name: "faults", class: "faultInfo"},
-	request{name: "capacity-rules", class: "fvcapRule"},
 	request{
-		name:  "ep-count",
-		class: "fvEpP",
-		query: []string{"rsp-subtree-include=count"},
+		name:   "faults",
+		class:  "faultInfo",
+		filter: "#.faultInst.attributes",
 	},
 	request{
-		name:  "ip-count",
-		class: "fvIp",
-		query: []string{"rsp-subtree-include=count"},
+		name:   "capacity-rules",
+		class:  "fvcapRule",
+		filter: "#.fvcapRule.attributes",
 	},
 	request{
-		name:  "l4l7-container-count",
-		class: "vnsCDev",
-		query: []string{"rsp-subtree-include=count"},
+		name:   "ep-count",
+		class:  "fvEpP",
+		query:  []string{"rsp-subtree-include=count"},
+		filter: "0.moCount.attributes",
 	},
 	request{
-		name:  "l4l7-service-graph-count",
-		class: "vnsGraphInst",
-		query: []string{"rsp-subtree-include=count"},
+		name:   "ip-count",
+		class:  "fvIp",
+		query:  []string{"rsp-subtree-include=count"},
+		filter: "0.moCount.attributes",
 	},
 	request{
-		name:  "mo-count-by-node",
-		class: "ctxClassCnt",
-		query: []string{"rsp-subtree-class=l2BD,fvEpP,l3Dom"},
+		name:   "l4l7-container-count",
+		class:  "vnsCDev",
+		query:  []string{"rsp-subtree-include=count"},
+		filter: "0.moCount.attributes",
 	},
-	request{name: "capacity-vlan", class: "eqptcapacityVlanUsage5min"},
-	request{name: "capacity-tcam", class: "eqptcapacityPolUsage5min"},
-	request{name: "capacity-l2-local", class: "eqptcapacityL2Usage5min"},
-	request{name: "capacity-l2-remote", class: "eqptcapacityL2RemoteUsage5min"},
-	request{name: "capacity-l2-total", class: "eqptcapacityL2TotalUsage5min"},
-	request{name: "capacity-l3-local", class: "eqptcapacityL3Usage5min"},
-	request{name: "capacity-l3-remote", class: "eqptcapacityL3RemoteUsage5min"},
-	request{name: "capacity-l3-total", class: "eqptcapacityL3TotalUsage5min"},
-	request{name: "capacity-l3-local-cap", class: "eqptcapacityL3UsageCap5min"},
-	request{name: "capacity-l3-remote-cap", class: "eqptcapacityL3RemoteUsageCap5min"},
-	request{name: "capacity-l3-total-cap", class: "eqptcapacityL3TotalUsageCap5min"},
-	request{name: "capacity-mcast", class: "eqptcapacityMcastUsage5min"},
+	request{
+		name:   "l4l7-service-graph-count",
+		class:  "vnsGraphInst",
+		query:  []string{"rsp-subtree-include=count"},
+		filter: "0.moCount.attributes",
+	},
+	request{
+		name:   "mo-count-by-node",
+		class:  "ctxClassCnt",
+		query:  []string{"rsp-subtree-class=l2BD,fvEpP,l3Dom"},
+		filter: "#.ctxClassCnt.attributes",
+	},
+	request{
+		name:   "capacity-vlan",
+		class:  "eqptcapacityVlanUsage5min",
+		filter: "#.eqptcapacityVlanUsage5min.attributes",
+	},
+	request{
+		name:   "capacity-tcam",
+		class:  "eqptcapacityPolUsage5min",
+		filter: "#.eqptcapacityPolUsage5min.attributes",
+	},
+	request{
+		name:   "capacity-l2-local",
+		class:  "eqptcapacityL2Usage5min",
+		filter: "#.eqptcapacityL2Usage5min.attributes",
+	},
+	request{
+		name:   "capacity-l2-remote",
+		class:  "eqptcapacityL2RemoteUsage5min",
+		filter: "#.eqptcapacityL2RemoteUsage5min.attributes",
+	},
+	request{
+		name:   "capacity-l2-total",
+		class:  "eqptcapacityL2TotalUsage5min",
+		filter: "#.eqptcapacityL2TotalUsage5min.attributes",
+	},
+	request{
+		name:   "capacity-l3-local",
+		class:  "eqptcapacityL3Usage5min",
+		filter: "#.eqptcapacityL3Usage5min.attributes",
+	},
+	request{
+		name:   "capacity-l3-remote",
+		class:  "eqptcapacityL3RemoteUsage5min",
+		filter: "#.eqptcapacityL3RemoteUsage5min.attributes",
+	},
+	request{
+		name:   "capacity-l3-total",
+		class:  "eqptcapacityL3TotalUsage5min",
+		filter: "#.eqptcapacityL3TotalUsage5min.attributes",
+	},
+	request{
+		name:   "capacity-l3-local-cap",
+		class:  "eqptcapacityL3UsageCap5min",
+		filter: "#.eqptcapacityL3UsageCap5min.attributes",
+	},
+	request{
+		name:   "capacity-l3-remote-cap",
+		class:  "eqptcapacityL3RemoteUsageCap5min",
+		filter: "#.eqptcapacityL3RemoteUsageCap5min.attributes",
+	},
+	request{
+		name:   "capacity-l3-total-cap",
+		class:  "eqptcapacityL3TotalUsageCap5min",
+		filter: "#.eqptcapacityL3TotalUsageCap5min.attributes",
+	},
+	request{
+		name:   "capacity-mcast",
+		class:  "eqptcapacityMcastUsage5min",
+		filter: "#.eqptcapacityMcastUsage5min.attributes",
+	},
 
 	// Global config
-	request{name: "bgp-route-reflectors", class: "bgpRRNodePEp"},
-	request{name: "crypto-key", class: "pkiExportEncryptionKey"},
-	request{name: "ep-loop-control", class: "epLoopProtectP"},
-	request{name: "fabric-wide-settings", class: "infraSetPol"},
-	request{name: "ip-aging", class: "epIpAgingP"},
-	request{name: "mcp-global", class: "mcpInstPol"},
-	request{name: "mcp-interface", class: "mcpIfPol"},
-	request{name: "port-tracking", class: "infraPortTrackPol"},
-	request{name: "rogue-ep-control", class: "epControlP"},
+	request{
+		name:   "bgp-route-reflectors",
+		class:  "bgpRRNodePEp",
+		filter: "#.bgpRRNodePEp.attributes",
+	},
+	request{
+		name:   "crypto-key",
+		class:  "pkiExportEncryptionKey",
+		filter: "0.pkiExportEncryptionKey.attributes",
+	},
+	request{
+		name:   "ep-loop-control",
+		class:  "epLoopProtectP",
+		filter: "0.epLoopProtectP.attributes",
+	},
+	request{
+		name:   "fabric-wide-settings",
+		class:  "infraSetPol",
+		filter: "0.infraSetPol.attributes",
+	},
+	request{
+		name:   "ip-aging",
+		class:  "epIpAgingP",
+		filter: "0.epIpAgingP.attributes",
+	},
+	request{
+		name:   "mcp-global",
+		class:  "mcpInstPol",
+		filter: "0.mcpInstPol.attributes",
+	},
+	request{
+		name:   "mcp-interface",
+		class:  "mcpIfPol",
+		filter: "#.mcpIfPol.attributes",
+	},
+	request{
+		name:   "port-tracking",
+		class:  "infraPortTrackPol",
+		filter: "0.infraPortTrackPol.attributes",
+	},
+	request{
+		name:   "rogue-ep-control",
+		class:  "epControlP",
+		filter: "0.epControlP.attributes",
+	},
 }
 
-func writeFile(name string, body []byte) string {
-	fn := fmt.Sprintf("%s.json", name)
-	if err := ioutil.WriteFile(fn, body, 0644); err != nil {
-		log.Panic(err)
-	}
-	return fn
-}
-
-func fetch(client aci.Client, cfg Config, req request, c chan string) {
+func fetch(client aci.Client, req request) {
 	fmt.Printf("fetching %s\n", req.name)
 	res, err := client.Get(aci.Req{
 		URI:   fmt.Sprintf("/api/class/%s", req.class),
@@ -139,19 +298,14 @@ func fetch(client aci.Client, cfg Config, req request, c chan string) {
 		fmt.Printf("%+v\n", req)
 		log.Fatal(err)
 	}
-	data := res.Raw
-	if cfg.Pretty {
-		data = res.Get("@pretty").Raw
-	}
-	c <- writeFile(req.name, []byte(data))
+
+	mutex.Lock()
+	out, _ = sjson.SetRawBytes(out, req.name, []byte(res.Get(req.filter).Raw))
+	mutex.Unlock()
+	wg.Done()
 }
 
 func zipFiles(files []string) {
-	fmt.Println("creating archive")
-	os.Remove(resultZip) // Remove any old archives and ignore errors
-	if err := archiver.Archive(files, resultZip); err != nil {
-		log.Panic(err)
-	}
 }
 
 func rmTempFiles(files []string) {
@@ -181,27 +335,37 @@ func main() {
 
 	// Fetch data from API
 	fmt.Println(strings.Repeat("=", 30))
-	var files []string
-	c := make(chan string)
 	for _, req := range reqs {
-		go fetch(client, cfg, req, c)
+		wg.Add(1)
+		go fetch(client, req)
 	}
-	for range reqs {
-		files = append(files, <-c)
-	}
+	wg.Wait()
+
 	fmt.Println(strings.Repeat("=", 30))
 
-	// Append metadata
+	// Add metadata
 	metadata, _ := json.Marshal(map[string]interface{}{
 		"collectorVersion": version,
 		"schemaVersion":    schemaVersion,
 		"timestamp":        time.Now(),
 	})
-	files = append(files, writeFile("meta", metadata))
+	out, _ = sjson.SetRawBytes(out, "meta", metadata)
+
+	// Create output file
+
+	if err := ioutil.WriteFile(resultFile, []byte(out), 0644); err != nil {
+		log.Fatalln("could not create file", resultFile)
+	}
 
 	// Create archive
-	zipFiles(files)
-	rmTempFiles(files)
+	fmt.Println("creating archive")
+	os.Remove(resultZip) // Remove any old archives and ignore errors
+	if err := archiver.Archive([]string{resultFile}, resultZip); err != nil {
+		log.Panic(err)
+	}
+
+	// Cleanup
+	os.Remove(resultFile)
 	fmt.Println(strings.Repeat("=", 30))
 	fmt.Println("Collection complete.")
 	fmt.Printf("Please provide %s to Cisco services for further analysis.\n", resultZip)
