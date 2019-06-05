1f59c44b49635cddc779b19ac17392e92a3365ef
Nathan Hemingway <nhemingw@cisco.com>
2019-06-04 19:51:29 -0700

    update build script to format of aci-vet

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+echo ============================== Test
+go test
+
+echo ============================== Build
 gox -os="linux darwin windows" -arch="amd64" -output="aci-collector.{{.OS}}" -verbose ./...
+
+echo ============================== Done
