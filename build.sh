acdba6cfe50856fa023890c03b1ba866cfffe070
Nathan Hemingway <nhemingw@cisco.com>
2019-06-05 19:47:56 -0700

    remove build script and use gorelease

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo ============================== Test
-go test
-
-echo ============================== Build
-gox -os="linux darwin windows" -arch="amd64" -output="aci-collector.{{.OS}}" -verbose ./...
-
-echo ============================== Done
