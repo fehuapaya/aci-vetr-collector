d433e708df7d865feb4e2ee3473d8bb5cbb00370
Nathan Hemingway <nathan@planfu.com>
2019-12-11 18:43:20 -0800

    +tests for fetch

--- go.mod
+++ go.mod
@@ -12,11 +12,14 @@ require (
 	github.com/nwaples/rardecode v1.0.0 // indirect
 	github.com/pierrec/lz4 v2.0.5+incompatible // indirect
 	github.com/rs/zerolog v1.14.3
+	github.com/stretchr/testify v1.3.0
 	github.com/tidwall/btree v0.0.0-20170113224114-9876f1454cf0 // indirect
 	github.com/tidwall/buntdb v1.1.0
+	github.com/tidwall/gjson v1.3.4
 	github.com/tidwall/grect v0.0.0-20161006141115-ba9a043346eb // indirect
 	github.com/tidwall/rtree v0.0.0-20180113144539-6cd427091e0e // indirect
 	github.com/tidwall/tinyqueue v0.0.0-20180302190814-1e39f5511563 // indirect
 	github.com/xi2/xz v0.0.0-20171230120015-48954b6210f8 // indirect
 	golang.org/x/sys v0.0.0-20190515190549-87c872767d25 // indirect
+	gopkg.in/h2non/gock.v1 v1.0.15
 )
---
+++ main_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"testing"
+	"time"
+
+	"github.com/brightpuddle/goaci"
+	"github.com/stretchr/testify/assert"
+	"github.com/tidwall/buntdb"
+	"github.com/tidwall/gjson"
+	"gopkg.in/h2non/gock.v1"
+)
+
+func TestFetch(t *testing.T) {
+	defer gock.Off()
+	gock.New("https://apic").
+		Get("/api/class/fvTenant.json").
+		Reply(200).
+		BodyString(goaci.Body{}.
+			Set("imdata.0.fvTenant.attributes.dn", "uni/tn-zero").
+			Set("imdata.1.fvTenant.attributes.dn", "uni/tn-one").
+			Str)
+
+	client, _ := goaci.NewClient("apic", "usr", "pwd")
+	client.LastRefresh = time.Now()
+	gock.InterceptClient(client.HttpClient)
+	db, _ := buntdb.Open(":memory:")
+	req := Request{class: "fvTenant"}
+	wg.Add(1)
+	fetch(client, req, db)
+	err := db.View(func(tx *buntdb.Tx) error {
+		return tx.AscendKeys("fvTenant:*", func(key, value string) bool {
+			assert.Equal(t, key, "fvTenant:"+gjson.Get(value, "dn").Str)
+			return true
+		})
+	})
+	assert.NoError(t, err)
+
+	// name     string             // Custom class name for DB - use class by default
+	// class    string             // MO class to query
+	// queries  []func(*goaci.Req) // query paramters
+	// filter   string             // GJSON path query for the result
+	// optional bool               // Fail on unsuccessful collection?
+}
