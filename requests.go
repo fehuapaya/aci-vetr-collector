846c6aaedc03587930b1448cc7a8d3a0bd34844b
Nathan Hemingway <nathan@planfu.com>
2020-01-15 15:23:58 -0800

    update requests structure

--- requests.go
+++ requests.go
@@ -1,133 +1,172 @@
 package main
 
-import "github.com/brightpuddle/goaci"
+import (
+	"fmt"
+
+	"github.com/brightpuddle/goaci"
+)
+
+type Mod = func(*goaci.Req)
 
 // Request is an HTTP request.
 type Request struct {
-	req    goaci.Req // goACI request object
-	prefix string    // Prefix for the DB
+	class  string // MO class
+	path   string // Request path
+	prefix string // Prefix for the DB
+	mods   []Mod  // Request modifiers, e.g. query parameters
+	filter string // Result filter (default to #.{class}.attributes)
 }
 
-// Create new request and use classname as db prefix
-func newRequest(class string, mods ...func(*goaci.Req)) Request {
-	req := goaci.NewReq("GET", "api/class/"+class, nil, mods...)
-	return Request{req: req, prefix: class}
-}
+func getRequests() []*Request {
+	reqs := []*Request{
+		/************************************************************
+		Infrastructure
+		************************************************************/
+		{class: "topSystem"},    // All devices
+		{class: "eqptBoard"},    // APIC hardware
+		{class: "fabricNode"},   // Switch hardware
+		{class: "fabricSetupP"}, // Pods (fabric setup policy)
 
-var reqs = []Request{
-	/************************************************************
-	Infrastructure
-	************************************************************/
-	newRequest("topSystem"),    // All devices
-	newRequest("eqptBoard"),    // APIC hardware
-	newRequest("fabricNode"),   // Switch hardware
-	newRequest("fabricSetupP"), // Pods (fabric setup policy)
+		/************************************************************
+		Fabric-wide settings
+		************************************************************/
+		{class: "epLoopProtectP"},    // EP loop protection policy
+		{class: "epControlP"},        // Rogue EP control policy
+		{class: "epIpAgingP"},        // IP aging policy
+		{class: "infraSetPol"},       // Fabric-wide settings
+		{class: "infraPortTrackPol"}, // Port tracking policy
+		{class: "coopPol"},           // COOP group policy
 
-	/************************************************************
-	Fabric-wide settings
-	************************************************************/
-	newRequest("epLoopProtectP"),    // EP loop protection policy
-	newRequest("epControlP"),        // Rogue EP control policy
-	newRequest("epIpAgingP"),        // IP aging policy
-	newRequest("infraSetPol"),       // Fabric-wide settings
-	newRequest("infraPortTrackPol"), // Port tracking policy
-	newRequest("coopPol"),           // COOP group policy
+		/************************************************************
+		Tenants
+		************************************************************/
+		// Primary constructs
+		{class: "fvAEPg"},   // EPG
+		{class: "fvRsBd"},   // EPG --> BD
+		{class: "fvBD"},     // BD
+		{class: "fvCtx"},    // VRF
+		{class: "fvTenant"}, // Tenant
+		{class: "fvSubnet"}, // Subnet
 
-	/************************************************************
-	Tenants
-	************************************************************/
-	// Primary constructs
-	newRequest("fvAEPg"),   // EPG
-	newRequest("fvRsBd"),   // EPG --> BD
-	newRequest("fvBD"),     // BD
-	newRequest("fvCtx"),    // VRF
-	newRequest("fvTenant"), // Tenant
-	newRequest("fvSubnet"), // Subnet
+		// Contracts
+		{class: "vzBrCP"},          // Contract
+		{class: "vzFilter"},        // Filter
+		{class: "vzSubj"},          // Subject
+		{class: "vzRsSubjFiltAtt"}, // Subject --> filter
+		{class: "fvRsProv"},        // EPG --> contract provided
+		{class: "fvRsCons"},        // EPG --> contract consumed
 
-	// Contracts
-	newRequest("vzBrCP"),          // Contract
-	newRequest("vzFilter"),        // Filter
-	newRequest("vzSubj"),          // Subject
-	newRequest("vzRsSubjFiltAtt"), // Subject --> filter
-	newRequest("fvRsProv"),        // EPG --> contract provided
-	newRequest("fvRsCons"),        // EPG --> contract consumed
+		// L3outs
+		{class: "l3extOut"},            // L3out
+		{class: "l3extLNodeP"},         // L3 node profile
+		{class: "l3extRsNodeL3OutAtt"}, // Node profile --> Node
+		{class: "l3extLIfP"},           // L3 interface profile
+		{class: "l3extInstP"},          // External EPG
 
-	// L3outs
-	newRequest("l3extOut"),            // L3out
-	newRequest("l3extLNodeP"),         // L3 node profile
-	newRequest("l3extRsNodeL3OutAtt"), // Node profile --> Node
-	newRequest("l3extLIfP"),           // L3 interface profile
-	newRequest("l3extInstP"),          // External EPG
+		/************************************************************
+		Fabric Policies
+		************************************************************/
+		{class: "isisDomPol"},         // ISIS policy
+		{class: "bgpRRNodePEp"},       // BGP route reflector nodes
+		{class: "l3IfPol"},            // L3 interface policy
+		{class: "fabricNodeControl"},  // node control (Dom, netflow,etc)
+		{class: "fabricRsNodeCtrl"},   // node policy group --> node control
+		{class: "fabricRsLeNodePGrp"}, // leaf --> leaf node policy group
+		{class: "fabricNodeBlk"},      // Node block
 
-	/************************************************************
-	Fabric Policies
-	************************************************************/
-	newRequest("isisDomPol"),         // ISIS policy
-	newRequest("bgpRRNodePEp"),       // BGP route reflector nodes
-	newRequest("l3IfPol"),            // L3 interface policy
-	newRequest("fabricNodeControl"),  // node control (Dom, netflow,etc)
-	newRequest("fabricRsNodeCtrl"),   // node policy group --> node control
-	newRequest("fabricRsLeNodePGrp"), // leaf --> leaf node policy group
-	newRequest("fabricNodeBlk"),      // Node block
+		/************************************************************
+		Fabric Access
+		************************************************************/
+		// MCP
+		{class: "mcpIfPol"},          // MCP inteface policy
+		{class: "infraRsMcpIfPol"},   // MCP pol --> policy group
+		{class: "infraRsAccBaseGrp"}, // policy group --> host port selector
+		{class: "infraRsAccPortP"},   // int profile --> node profile
 
-	/************************************************************
-	Fabric Access
-	************************************************************/
-	// MCP
-	newRequest("mcpIfPol"),          // MCP inteface policy
-	newRequest("infraRsMcpIfPol"),   // MCP pol --> policy group
-	newRequest("infraRsAccBaseGrp"), // policy group --> host port selector
-	newRequest("infraRsAccPortP"),   // int profile --> node profile
+		{class: "mcpInstPol"}, // MCP global policy
 
-	newRequest("mcpInstPol"), // MCP global policy
+		// AEP/domain/VLANs
+		{class: "infraAttEntityP"}, // AEP
+		{class: "infraRsDomP"},     // AEP --> domain
+		{class: "infraRsVlanNs"},   // Domain --> VLAN pool
+		{class: "fvnsEncapBlk"},    // VLAN encap block
 
-	// AEP/domain/VLANs
-	newRequest("infraAttEntityP"), // AEP
-	newRequest("infraRsDomP"),     // AEP --> domain
-	newRequest("infraRsVlanNs"),   // Domain --> VLAN pool
-	newRequest("fvnsEncapBlk"),    // VLAN encap block
+		/************************************************************
+		Admin/Operations
+		************************************************************/
+		{class: "firmwareRunning"},        // Switch firmware
+		{class: "firmwareCtrlrRunning"},   // Controller firmware
+		{class: "pkiExportEncryptionKey"}, // Crypto key
 
-	/************************************************************
-	Admin/Operations
-	************************************************************/
-	newRequest("firmwareRunning"),        // Switch firmware
-	newRequest("firmwareCtrlrRunning"),   // Controller firmware
-	newRequest("pkiExportEncryptionKey"), // Crypto key
+		/************************************************************
+		Live State
+		************************************************************/
+		{class: "faultInst"}, // Faults
+		{class: "fvcapRule"}, // Capacity rules
 
-	/************************************************************
-	Live State
-	************************************************************/
-	newRequest("faultInst"), // Faults
-	newRequest("fvcapRule"), // Capacity rules
-	// Endpoint count
-	newRequest("fvCEp", goaci.Query("rsp-subtree-include", "count")),
-	// IP count
-	newRequest("fvIp", goaci.Query("rsp-subtree-include", "count")),
-	// L4-L7 container count
-	newRequest("vnsCDev", goaci.Query("rsp-subtree-include", "count")),
-	// L4-L7 service graph count
-	newRequest("vnsGraphInst", goaci.Query("rsp-subtree-include", "count")),
-	// MO count by node
-	newRequest("ctxClassCnt", goaci.Query("rsp-subtree-class", "l2BD,fvEpP,l3Dom")),
+		{ // Endpoint count
+			class:  "fvCEp",
+			filter: "#.moCount.attributes",
+			mods:   []Mod{goaci.Query("rsp-subtree-include", "count")},
+		},
+		{ // IP count
+			class:  "fvIp",
+			filter: "#.moCount.attributes",
+			mods:   []Mod{goaci.Query("rsp-subtree-include", "count")},
+		},
 
-	// Fabric health
-	newRequest("fabricHealthTotal"), // Total and per-pod health scores
-	{ // Per-device health stats
-		req:    newRequest("topSystem", goaci.Query("rsp-subtree-include", "health,no-scoped")).req,
-		prefix: "healthInst",
-	},
+		{ // L4-L7 container count
+			class:  "vnsCDev",
+			filter: "#.moCount.attributes",
+			mods:   []Mod{goaci.Query("rsp-subtree-include", "count")},
+		},
 
-	// Switch capacity
-	newRequest("eqptcapacityVlanUsage5min"),        // VLAN
-	newRequest("eqptcapacityPolUsage5min"),         // TCAM
-	newRequest("eqptcapacityL2Usage5min"),          // L2 local
-	newRequest("eqptcapacityL2RemoteUsage5min"),    // L2 remote
-	newRequest("eqptcapacityL2TotalUsage5min"),     // L2 total
-	newRequest("eqptcapacityL3Usage5min"),          // L3 local
-	newRequest("eqptcapacityL3UsageCap5min"),       // L3 local cap
-	newRequest("eqptcapacityL3RemoteUsage5min"),    // L3 remote
-	newRequest("eqptcapacityL3RemoteUsageCap5min"), // L3 remote cap
-	newRequest("eqptcapacityL3TotalUsage5min"),     // L3 total
-	newRequest("eqptcapacityL3TotalUsageCap5min"),  // L3 total cap
-	newRequest("eqptcapacityMcastUsage5min"),       // Multicast
+		{ // L4-L7 service graph count
+			class:  "vnsGraphInst",
+			filter: "#.moCount.attributes",
+			mods:   []Mod{goaci.Query("rsp-subtree-include", "count")},
+		},
+
+		{ // MO count by node
+			class:  "ctxClassCnt",
+			filter: "#.moCount.attributes",
+			mods:   []Mod{goaci.Query("rsp-subtree-class", "l2BD,fvEpP,l3Dom")},
+		},
+
+		// Fabric health
+		{class: "fabricHealthTotal"}, // Total and per-pod health scores
+		{ // Per-device health stats
+			class:  "topSystem",
+			prefix: "heatlhInst",
+			mods:   []Mod{goaci.Query("rsp-subtree-include", "health,no-scoped")},
+			filter: "#.attributes.healthInst",
+		},
+
+		// Switch capacity
+		{class: "eqptcapacityVlanUsage5min"},        // VLAN
+		{class: "eqptcapacityPolUsage5min"},         // TCAM
+		{class: "eqptcapacityL2Usage5min"},          // L2 local
+		{class: "eqptcapacityL2RemoteUsage5min"},    // L2 remote
+		{class: "eqptcapacityL2TotalUsage5min"},     // L2 total
+		{class: "eqptcapacityL3Usage5min"},          // L3 local
+		{class: "eqptcapacityL3UsageCap5min"},       // L3 local cap
+		{class: "eqptcapacityL3RemoteUsage5min"},    // L3 remote
+		{class: "eqptcapacityL3RemoteUsageCap5min"}, // L3 remote cap
+		{class: "eqptcapacityL3TotalUsage5min"},     // L3 total
+		{class: "eqptcapacityL3TotalUsageCap5min"},  // L3 total cap
+		{class: "eqptcapacityMcastUsage5min"},       // Multicast
+	}
+
+	for _, req := range reqs {
+		if req.filter == "" {
+			req.filter = fmt.Sprintf("#.%s.attributes", req.class)
+		}
+		if req.path == "" {
+			req.path = "/api/class/" + req.class
+		}
+		if req.prefix == "" {
+			req.prefix = req.class
+		}
+	}
+	return reqs
 }
